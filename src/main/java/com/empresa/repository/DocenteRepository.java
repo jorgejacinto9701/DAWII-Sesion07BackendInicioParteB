ed482753f1af5aac58624e8057dd8d099c9ff3a7
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2022-10-17 12:26:25 -0500

    Ajustes

--- src/main/java/com/empresa/repository/DocenteRepository.java
+++ src/main/java/com/empresa/repository/DocenteRepository.java
@@ -3,7 +3,6 @@ package com.empresa.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.empresa.entity.Docente;
 
@@ -12,7 +11,7 @@ public interface DocenteRepository extends JpaRepository<Docente, Integer> {
 	//JPQL
 	//Query no con tablas sino con clases que tienen @Entity
 	
-	@Query("select x from Docente x where (?1 is '' or x.nombre like ?1) and (?2 is '' or x.dni = ?2) and (?3 is -1 or x.ubigeo.idUbigeo = ?3) and x.estado = ?4")       
+	       
 	public List<Docente> listaDocentePorNombreDniUbigeo(String nombre, String dni, int idUbigeo, int estado);
 	
 }

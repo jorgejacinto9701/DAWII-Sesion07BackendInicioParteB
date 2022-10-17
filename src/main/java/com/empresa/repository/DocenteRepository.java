a7d59d963a02f8375dab830372585497b0e7fcef
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2022-10-17 12:53:51 -0500

    Ajustes

--- src/main/java/com/empresa/repository/DocenteRepository.java
+++ src/main/java/com/empresa/repository/DocenteRepository.java
@@ -3,6 +3,7 @@ package com.empresa.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.empresa.entity.Docente;
 
@@ -11,7 +12,7 @@ public interface DocenteRepository extends JpaRepository<Docente, Integer> {
 	//JPQL
 	//Query no con tablas sino con clases que tienen @Entity
 	
-	       
+	@Query("select x from Docente x")       
 	public List<Docente> listaDocentePorNombreDniUbigeo(String nombre, String dni, int idUbigeo, int estado);
 	
 }
